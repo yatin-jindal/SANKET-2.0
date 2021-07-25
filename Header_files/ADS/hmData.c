ec05ea76e33a4fe224e98985d5ddb53054a7fbee
kriti628 <60791427+kriti628@users.noreply.github.com>
2021-07-25 17:32:22 +0530

    Update hmData.c

--- Header_files/ADS/hmData.c
+++ Header_files/ADS/hmData.c
@@ -1,18 +1,18 @@
 #include "adc.h"
 #include "hmData.h"
 
-extern uint8_t detSwtch1 = 0;
-extern uint8_t detSwtch2 = 0;
-extern uint8_t currLimOC = 0;
-extern uint8_t currLimEN = 0;
-extern hmDataADS[6];
+extern uint8_t detSwtch1;
+extern uint8_t detSwtch2;
+extern uint8_t currLimOC;
+extern uint8_t currLimEN;
+extern uint8_t hmDataADS[6];
 
 void hmDataCollect(void){
-	DetSwtch1 = PINA & (1<<PA3);    //0b00001000
-	DetSwtch2 = PINA & (1<<PA5);	//0b00100000
+	detSwtch1 = PINA & (1<<PA3);    //0b00001000
+	detSwtch2 = PINA & (1<<PA5);	//0b00100000
 	currLimOC = PINA & (1<<PA7);	//0b10000000
 	currLimEN = PINA &(1<<PA6);		//0b01000000
-	hmDataADS[0] =(DetSwtch1 >>3 )|(DetSwtch2 >>3)|(currLimEN >>2)|(currLimOC >>1) ;
+	hmDataADS[0] =(detSwtch1 >>3 )|(detSwtch2 >>3)|(currLimEN >>2)|(currLimOC >>1) ;
 
 	adcMeasure( 0x00 , 1 );
 	adcMeasure( 0x01 , 2 );
